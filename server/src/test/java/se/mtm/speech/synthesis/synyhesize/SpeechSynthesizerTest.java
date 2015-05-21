6b921db1cf43f67a353d856fb0159d948bc23de3
Thomas Sundberg <thomas.sundberg@mtm.se>
2015-05-21 13:13:38 +0200

    Verify that adding a sentence for synthesising returns false if the que is full

--- server/src/test/java/se/mtm/speech/synthesis/synyhesize/SpeechSynthesizerTest.java
+++ server/src/test/java/se/mtm/speech/synthesis/synyhesize/SpeechSynthesizerTest.java
@@ -1,18 +1,30 @@
 package se.mtm.speech.synthesis.synyhesize;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class SpeechSynthesizerTest {
+    private SpeechSynthesizer speechSynthesizer;
+
+    @Before
+    public void setUp() throws Exception {
+        speechSynthesizer = new SpeechSynthesizer(1);
+        speechSynthesizer.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        speechSynthesizer.stop();
+    }
 
     @Test
     public void synthezise_paragraph() throws Exception { // NOPMD
-        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
-        speechSynthesizer.start();
-
         String key = "17";
         Paragraph paragraph = new Paragraph(key, "A sentence to be synthesized");
 
@@ -24,12 +36,22 @@ public class SpeechSynthesizerTest {
 
         Paragraph actualParagraph = speechSynthesizer.getParagraph(key);
 
-        speechSynthesizer.stop();
-
         assertTrue("Expected to be able to deliver a paragraph for synthetization", actualDelivery);
         assertThat(actualParagraph, is(paragraph));
     }
 
+    @Test
+    public void add_too_many_sentences_and_verify_that_que_is_full() {
+        Paragraph paragraph = new Paragraph("17", "A sentence to be synthesized");
+
+        boolean firstDelivery = speechSynthesizer.addParagraph(paragraph);
+        boolean secondDelivery = speechSynthesizer.addParagraph(paragraph);
+
+        assertTrue("The que can accept one item", firstDelivery);
+        assertFalse("The que can accept one item", secondDelivery);
+    }
+
+
     // todo add a paragraph that isn't returned in time. A timeout should not be handled by returning null. Return a "timeout pargraph" instead
-    // todo too many paragraphs and verify the it isn't accepted
+    // todo add many paragraphs and verify the are consumed after a while
 }
