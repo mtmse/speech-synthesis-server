a2906e82cdb882219f331a2e8f1886ba5c9939f8
Thomas Sundberg <thomas.sundberg@mtm.se>
2015-05-22 13:13:28 +0200

    Use a JUnit rule instead for starting the application

--- server/src/integrationTest/java/se/mtm/speech/synthesis/SynthesizeIntegrationTest.java
+++ server/src/integrationTest/java/se/mtm/speech/synthesis/SynthesizeIntegrationTest.java
@@ -1,22 +1,22 @@
 package se.mtm.speech.synthesis;
 
-import org.junit.BeforeClass;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.Rule;
 import org.junit.Test;
+import se.mtm.speech.synthesis.infrastructure.Configuration;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SynthesizeIntegrationTest {
-    private static int port;
-
-    @BeforeClass // NOPMD
-    public static void setUp() throws Exception {
-        Main application = ApplicationRunner.runApplication();
-        port = application.getHttpPort();
-    }
+    @Rule
+    public final DropwizardAppRule<Configuration> application =
+            new DropwizardAppRule<>(Main.class, ResourceHelpers.resourceFilePath("test-configuration.yaml"));
 
     @Test
     public void synthesize_a_sentence() throws Exception { // NOPMD
+        int port = application.getLocalPort();
         SpeechClient client = new SpeechClient("localhost", port);
 
         Paragraph actual = client.synthesise("Hello Filibuster!");
