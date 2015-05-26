c53c6e983d6cd122f8769568a1c6e09e120dff76
Thomas Sundberg <tsu@kth.se>
2015-05-26 16:24:33 +0200

    Removed deprecated classes

--- server/src/main/java/se/mtm/speech/synthesis/synyhesize/ParagraphNotReady.java
+++
@@ -1,5 +0,0 @@
-package se.mtm.speech.synthesis.synyhesize;
-
-@Deprecated
-public class ParagraphNotReady implements Paragraph {
-}
--- server/src/main/java/se/mtm/speech/synthesis/synyhesize/ParagraphTimedOut.java
+++
@@ -1,5 +0,0 @@
-package se.mtm.speech.synthesis.synyhesize;
-
-@Deprecated
-public class ParagraphTimedOut implements Paragraph {
-}
