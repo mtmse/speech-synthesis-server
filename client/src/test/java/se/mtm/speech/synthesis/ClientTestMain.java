25223f0c07adfc124edeec9e6d931bd82a8452a9
Thomas Sundberg <tsu@kth.se>
2015-06-30 14:41:03 +0200

    Run multiple clients in parallel

--- client/src/test/java/se/mtm/speech/synthesis/ClientTestMain.java
+++ client/src/test/java/se/mtm/speech/synthesis/ClientTestMain.java
@@ -4,6 +4,8 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public final class ClientTestMain {
 
@@ -11,32 +13,66 @@ public final class ClientTestMain {
         // utility class
     }
 
-    public static void main(String... args) throws IOException {
-        String sentence = "Kollar att speech servern kan svara på tilltal och göra text till ljud.";
-        String fileName = "result.wav";
+    public static void main(String... args) throws Exception {
+        int numberOfClients = 15;
+        int testTimeInMinutes = 1;
 
-        SpeechClient client = getClient();
+        long executionTime = System.currentTimeMillis() + 1000 * 60 * testTimeInMinutes;
+        List<LoadClient> clients = new LinkedList<>();
+        while (clients.size() < numberOfClients) {
+            LoadClient client = new LoadClient();
+            clients.add(client);
+            new Thread(client).start();
+        }
 
-        SynthesizedSound sound = client.synthesise(sentence);
+        while (System.currentTimeMillis() < executionTime) {
+            Thread.sleep(500);
+        }
 
-        if (sound.isTimeout()) {
-            System.out.println("Timed out"); // NOPMD
-        } else {
-            writeWavFile(sound, fileName);
+        for (LoadClient client : clients) {
+            client.work = false;
         }
     }
 
-    private static SpeechClient getClient() {
-        String host = "pipeutv1.mtm.se";
-        int port = 9090;
+    static class LoadClient implements Runnable {
+        private boolean work = true;
 
-        return new SpeechClient(host, port);
+        @Override
+        public void run() {
+            SpeechClient client = getClient();
+            while (work) {
+                try {
+                    testSynthesiseSound(client);
+                } catch (IOException e) {
+                    client = getClient();
+                    e.printStackTrace(); // NOPMD
+                }
+            }
+        }
+
+        private void testSynthesiseSound(SpeechClient client) throws IOException {
+            String sentence = "Kollar att speech servern kan svara på tilltal och kan göra text till ljud.";
+
+            SynthesizedSound sound = client.synthesise(sentence);
+
+            if (sound.isTimeout()) {
+                System.out.print("t"); // NOPMD
+            } else {
+                System.out.print("."); // NOPMD
+            }
+        }
+
+        private SpeechClient getClient() {
+            String host = "pipeutv1.mtm.se";
+            int port = 9090;
+
+            return new SpeechClient(host, port);
+        }
+
+        private void writeWavFile(SynthesizedSound sound, String fileName) throws IOException {
+            File soundFile = new File(fileName);
+            byte[] content = sound.getSound();
+            FileUtils.writeByteArrayToFile(soundFile, content);
+        }
     }
-
-    private static void writeWavFile(SynthesizedSound sound, String fileName) throws IOException {
-        File soundFile = new File(fileName);
-        byte[] content = sound.getSound();
-        FileUtils.writeByteArrayToFile(soundFile, content);
-    }
-
 }
