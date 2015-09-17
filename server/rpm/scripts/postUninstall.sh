8a2811b83fe3a0f3444071a942e7e8e854635d15
Thomas Sundberg <tsu@kth.se>
2015-09-17 13:02:55 +0200

    Disabled installation logic while trouble shooting upgrade using Puppet

--- server/rpm/scripts/postUninstall.sh
+++ server/rpm/scripts/postUninstall.sh
@@ -4,24 +4,24 @@
 # !!!!! Observe that the output from this script might not be output to screen since yum deas NOT print it out (due to a bug in yum). Direct RPM execution should work, though.
 echo "Running postUninstall"
 
-if [ "$1" = "0" ]; then # This is a complete uninstallation
-# Perform tasks for cleanup after ultimate uninstallation (files are already deleted)
-	
-	# Save the machine specific config file in case we want a reinstallation soon
-	rm -f /etc/init.d/speech-synthesis-server
-	rm -rf /opt/speech-synthesis-server
-
-elif [ "$1" = "1" ]; then # This rpm is being replaced by another rpm (upgrade)
-# Perform whatever maintenance must occur after an upgrade (to a later version than this one) is finished (!). 
-# Observe!!! Executed AFTER the new rpm package has executed the install/upgrade scripts!!!!
-
-	# Currently no cleansing actions efter an upgrade from this version to the (unknown) next
-    echo "postUninstal This rpm is being replaced by another rpm (upgrade)"
-else # Unrecognized action type
-    echo "Invalid/unsupported action type: $1"
-    exit 1
-fi
-
-service speech-synthesis-server stop
+# if [ "$1" = "0" ]; then # This is a complete uninstallation
+# # Perform tasks for cleanup after ultimate uninstallation (files are already deleted)
+#
+# 	# Save the machine specific config file in case we want a reinstallation soon
+# 	rm -f /etc/init.d/speech-synthesis-server
+# 	rm -rf /opt/speech-synthesis-server
+#
+# elif [ "$1" = "1" ]; then # This rpm is being replaced by another rpm (upgrade)
+# # Perform whatever maintenance must occur after an upgrade (to a later version than this one) is finished (!).
+# # Observe!!! Executed AFTER the new rpm package has executed the install/upgrade scripts!!!!
+#
+# 	# Currently no cleansing actions efter an upgrade from this version to the (unknown) next
+#     echo "postUninstal This rpm is being replaced by another rpm (upgrade)"
+# else # Unrecognized action type
+#     echo "Invalid/unsupported action type: $1"
+#     exit 1
+# fi
+#
+# service speech-synthesis-server stop
 
 echo "Done running postUninstall"
