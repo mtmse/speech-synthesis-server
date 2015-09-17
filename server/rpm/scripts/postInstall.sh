5ac2aba58a9690525a546378086c5d9fca548678
Thomas Sundberg <tsu@kth.se>
2015-09-17 13:15:40 +0200

    Enabled preparing executable, config files and log files

--- server/rpm/scripts/postInstall.sh
+++ server/rpm/scripts/postInstall.sh
@@ -5,24 +5,23 @@
 
 echo "Running postInstall"
 
-# CONFIG_ROOT=/etc/opt/speech-synthesis-server
-# INSTALL_ROOT=/opt/speech-synthesis-server
-# LOGDIR=/var/log/mtm/speech-synthesis-server
-# RUN_AS_USER=tpbadmin
-#
-# if [ ! -f "$CONFIG_ROOT/configuration.yaml" ]; then
-#     echo "Wrote configuration.yaml from RPM"
-#     mkdir -p $CONFIG_ROOT
-# 	cp $INSTALL_ROOT/conf/configuration-prod.yaml $CONFIG_ROOT/configuration.yaml
-# fi
-#
-#
-# rm -f $INSTALL_ROOT/lib/server-all.jar
-# ln -s $INSTALL_ROOT/lib/server-*-all.jar $INSTALL_ROOT/lib/server-all.jar
-#
-# mkdir -p $LOGDIR
-# chown -R $RUN_AS_USER:$RUN_AS_USER $LOGDIR
-#
+CONFIG_ROOT=/etc/opt/speech-synthesis-server
+INSTALL_ROOT=/opt/speech-synthesis-server
+LOGDIR=/var/log/mtm/speech-synthesis-server
+RUN_AS_USER=tpbadmin
+
+if [ ! -f "$CONFIG_ROOT/configuration.yaml" ]; then
+    echo "Wrote configuration.yaml from RPM"
+    mkdir -p $CONFIG_ROOT
+	cp $INSTALL_ROOT/conf/configuration-prod.yaml $CONFIG_ROOT/configuration.yaml
+fi
+
+rm -f $INSTALL_ROOT/lib/server-all.jar
+ln -s $INSTALL_ROOT/lib/server-*-all.jar $INSTALL_ROOT/lib/server-all.jar
+
+mkdir -p $LOGDIR
+chown -R $RUN_AS_USER:$RUN_AS_USER $LOGDIR
+
 #
 # if [ "$1" = "1" ]; then # This is a new installation from scratch
 # # Perform tasks to prepare for the initial installation
