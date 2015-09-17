d56f512e523ded87f4e650c4691c9f700e39eefd
Thomas Sundberg <tsu@kth.se>
2015-09-17 14:22:34 +0200

    Be a good citizen and remove as much as possible on an uninstall

--- server/rpm/scripts/postUninstall.sh
+++ server/rpm/scripts/postUninstall.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "Running postUninstall"
+
+ACTION=$1
+
+UNINSTALL=0
+UPGRADE=1
+
+INSTALLATION_ROOT=/opt/speech-synthesis-server
+CONFIG_ROOT=/etc/opt/speech-synthesis-server
+LOGDIR=/var/log/mtm/speech-synthesis-server
+
+if [ $ACTION -eq $UNINSTALL ]; then
+    rm -rf $INSTALLATION_ROOT
+    rm -rf $CONFIG_ROOT
+    rm -rf $LOGDIR
+fi
+
+# if [ $ACTION -eq $INSTALL ]; then
+# fi
