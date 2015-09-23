527d7e0eff3eda0e86075ca2a9f2b525c5907bd0
Thomas Sundberg <tsu@kth.se>
2015-09-23 10:35:45 +0200

    Added debug information

--- server/rpm/scripts/utils.sh
+++ server/rpm/scripts/utils.sh
@@ -4,6 +4,8 @@ echo "Running utils"
 
 ACTION=$1
 
+echo "Action: $ACTION"
+
 UNINSTALL=0
 INSTALL_OR_UPGRADE=1
 
@@ -17,3 +19,5 @@ if [ $ACTION -eq $INSTALL_OR_UPGRADE ]; then
     rm -f $INSTALL_ROOT/lib/server-all.jar
     ln -s $INSTALL_ROOT/lib/server-*-all.jar $INSTALL_ROOT/lib/server-all.jar
 fi
+
+echo "Done running utils"
