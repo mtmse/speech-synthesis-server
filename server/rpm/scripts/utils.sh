19d0ac79ff23c460e7aa5ade0ae2b29fc3f8c549
Thomas Sundberg <tsu@kth.se>
2015-09-23 11:05:12 +0200

    Name the executable jar a good name as early as possible

--- server/rpm/scripts/utils.sh
+++ server/rpm/scripts/utils.sh
@@ -15,9 +15,4 @@ if [ $ACTION -eq $UNINSTALL ]; then
     service speech-synthesis-server stop
 fi
 
-if [ $ACTION -eq $INSTALL_OR_UPGRADE ]; then
-    rm -f $INSTALL_ROOT/lib/server-all.jar
-    ln -s $INSTALL_ROOT/lib/server-*-all.jar $INSTALL_ROOT/lib/server-all.jar
-fi
-
 echo "Done running utils"
