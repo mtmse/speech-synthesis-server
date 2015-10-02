c14a7f5201c10ad4f3c48ad33c68b820f3bfd200
Thomas Sundberg <tsu@kth.se>
2015-10-02 08:49:12 +0200

    Replaced duplication with a parametrized test

--- server/src/integrationTest/java/se/mtm/speech/synthesis/synthesize/SynthesizeIntegrationTest.java
+++ server/src/integrationTest/java/se/mtm/speech/synthesis/synthesize/SynthesizeIntegrationTest.java
@@ -4,16 +4,33 @@ import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import se.mtm.speech.synthesis.Main;
 import se.mtm.speech.synthesis.SpeechClient;
 import se.mtm.speech.synthesis.SynthesizedSound;
 import se.mtm.speech.synthesis.infrastructure.Configuration;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 import static junit.framework.TestCase.assertFalse;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+/**
+ * We have had issues with character encoding. The characters that gave us problems are represented in
+ * the parameters to this test. Add more examples if you experience problems with the character encoding.
+ */
+@RunWith(Parameterized.class)
 public class SynthesizeIntegrationTest {
+    private final String sentence;
+
+    public SynthesizeIntegrationTest(String sentence) {
+        this.sentence = sentence;
+    }
+
     @Rule
     public final DropwizardAppRule<Configuration> application =
             new DropwizardAppRule<>(Main.class, ResourceHelpers.resourceFilePath("test-configuration.yaml"));
@@ -23,7 +40,6 @@ public class SynthesizeIntegrationTest {
         int port = application.getLocalPort();
         SpeechClient client = new SpeechClient("localhost", port);
 
-        String sentence = "Hello Filibuster!";
         byte[] expectedSound = sentence.getBytes();
 
         SynthesizedSound actual = client.synthesise(sentence);
@@ -32,31 +48,14 @@ public class SynthesizeIntegrationTest {
         assertThat(actual.getSound(), is(expectedSound));
     }
 
-    @Test
-    public void synthesise_complicated_sentence() throws Exception {
-        int port = application.getLocalPort();
-        SpeechClient client = new SpeechClient("localhost", port);
+    @Parameterized.Parameters // NOPMD
+    public static Collection<String[]> testData() {
+        List<String[]> expectedTestData = new LinkedList<>();
 
-        String sentence = "tjänsteproduktionen (Hirschman 2008 {1970], s. 98–101).";
-        byte[] expectedSound = sentence.getBytes();
+        expectedTestData.add(new String[]{"Hello Filibuster!"});
+        expectedTestData.add(new String[]{"tjänsteproduktionen (Hirschman 2008 {1970], s. 98–101)."});
+        expectedTestData.add(new String[]{"60 %, är = 17"});
 
-        SynthesizedSound actual = client.synthesise(sentence);
-
-        assertFalse("The call should not have timed out", actual.isTimeout());
-        assertThat(actual.getSound(), is(expectedSound));
-    }
-
-    @Test
-    public void synthesise_sentence_with_percent() throws Exception {
-        int port = application.getLocalPort();
-        SpeechClient client = new SpeechClient("localhost", port);
-
-        String sentence = "60 %, är = 17";
-        byte[] expectedSound = sentence.getBytes();
-
-        SynthesizedSound actual = client.synthesise(sentence);
-
-        assertFalse("The call should not have timed out", actual.isTimeout());
-        assertThat(actual.getSound(), is(expectedSound));
+        return expectedTestData;
     }
 }
